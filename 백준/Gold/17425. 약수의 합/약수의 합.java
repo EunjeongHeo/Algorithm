f14501d4e5ebab4ec793e59cbb3aba9da75e97cd
Joy <151157127+EunjeongHeo@users.noreply.github.com>
2024-03-07 04:36:05 +0900

    Update 약수의 합.java

--- 백준/Gold/17425. 약수의 합/약수의 합.java
+++ 백준/Gold/17425. 약수의 합/약수의 합.java
@@ -1,31 +1,29 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 
 public class Main {
-    static final int MAX_NUM = 1000001;
     public static void main(String[] args) throws IOException {
-
-        //약수의 합 배열 생성
-        long[] nums = new long[MAX_NUM];
-        //약수의 합을 계산하여 배열에 저장
-        for (int i = 1; i < MAX_NUM; i++) {
-            int n = 1;
-            nums[i] += nums[i-1]; //이전까지의 약수의 합을 현재 약수의 합에 누적하여 저장
-            while (i * n < MAX_NUM) {
-                nums[i * n++] += i; //현재 수의 배수 인덱스에 현재수 합산
-            }
-        }
-
-        //반복횟수를 입력받고, 주어진 반복 횟수만큼 약수의 합을 찾아 출력
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        for (int n = 0; n < N; n++) {
-            sb.append(nums[Integer.parseInt(br.readLine())]).append("\n");
-        }
-        sb.setLength(sb.length() - 1);
-        System.out.println(sb);
 
+        long[] nums = new long[1000001];
+        Arrays.fill(nums, 1);
+
+        for (int i = 2; i <= 1000000; i++) {
+            //nums[i]에는 1부터 i-1까지의 값들의 배수에 해당된다면 그 값들이 합산되어 저장되어 있음.
+            for (int j = 1; i * j <= 1000000; j++) {
+                nums[i * j] += i; //i의 배수 번째 칸에 i 값 누적
+            }
+
+            //nums[i] = nums[i] + nums[i-1]
+            //g(x)    = f(x)    + g(x-1)
+            nums[i] += nums[i - 1]; //i번째 칸에 i-1번째 칸의 값 누적
+        }
+
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder answer = new StringBuilder();
+        while(n-- > 0) {
+            answer.append(nums[Integer.parseInt(br.readLine())]).append('\n');
+        }
+        System.out.println(answer);
     }
 }
