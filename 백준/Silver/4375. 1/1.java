5561e9ac29356b9d31acb78408a376c1a7fc7998
Joy <151157127+EunjeongHeo@users.noreply.github.com>
2024-03-07 04:39:37 +0900

    Update 1.java

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -2,25 +2,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class _4375 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringBuilder answer = new StringBuilder();
-
         String str;
         while((str = br.readLine()) != null){
             int n = Integer.parseInt(str);
-            int num = 1;
-            int count = 1;
-            while( num % n != 0 ){
+            int base = 1; //1, 11, .. 
+            int count = 1; //자릿수 카운트  
+            while(base % n != 0){
                 count++;
-                num = ( num * 10 + 1 ) % n;
-                num %= n;
+                base = (base * 10 + 1) % n;
             }
             answer.append(count).append('\n');
         }
-        System.out.print(answer);
-
+        System.out.println(answer);
     }
 }
