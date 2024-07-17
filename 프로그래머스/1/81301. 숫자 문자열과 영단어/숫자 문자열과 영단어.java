900ccfa9ff1bbcc66d997c4034963c00c6ba070f
Joy <151157127+EunjeongHeo@users.noreply.github.com>
2024-07-17 12:22:13 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.14 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,17 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public int solution(String s) {
-        
-        s = s.replace("zero","0");
-        s = s.replace("one","1");
-        s = s.replace("two","2");
-        s = s.replace("three","3");
-        s = s.replace("four","4");
-        s = s.replace("five","5");
-        s = s.replace("six","6");
-        s = s.replace("seven","7");
-        s = s.replace("eight","8");
-        s = s.replace("nine","9");
-        s = s.replace("ten","10");
+        Map<String, String> map = new HashMap<>();
+        map.put("zero", "0");
+        map.put("one", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+        map.put("four", "4");
+        map.put("five", "5");
+        map.put("six", "6");
+        map.put("seven", "7");
+        map.put("eight", "8");
+        map.put("nine", "9");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            s = s.replace(entry.getKey(), entry.getValue());
+        }
 
         int answer = Integer.parseInt(s);
         return answer;
