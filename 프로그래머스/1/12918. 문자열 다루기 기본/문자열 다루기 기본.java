3a669b3461737993949689c49df9c51721fca9e3
Joy <151157127+EunjeongHeo@users.noreply.github.com>
2024-07-17 13:02:17 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.13 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,20 +1,7 @@
 class Solution {
-    public boolean solution(String s) {
-        
-        char[] charArr = s.toCharArray();
-
-        // 문자열 길이 체크
-        if (charArr.length != 4 && charArr.length != 6){
-            return false;
-        }
-        
-        // 숫자 체크
-        for (char c : charArr){
-            if(!(c >= '0' && c <= '9'))
-                return false;
-        }
-        
-        return true;
-        
-    }
+  public boolean solution(String s) {
+        if (s.length() == 4 || s.length() == 6)
+		        return s.matches("(^[0-9]*$)");
+        return false;
+  }
 }
