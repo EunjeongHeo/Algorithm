f53f82f258e531d565a6b3987ef36447bbc53457
Joy <151157127+EunjeongHeo@users.noreply.github.com>
2024-07-17 11:12:43 +0900

    Update 이진 변환 반복하기.java

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -1,33 +1,23 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-
-
 class Solution {
-    public int[] solution(String S) {
-                
-        int count_zero = 0;
-        int count_translate = 0;
+    public int[] solution(String s) {
         
-        char[] s = S.toCharArray(); // 0 제거 전 문자열(문자리스트)
-        ArrayList<Character> x = new ArrayList<>(); // 0 제거 후 문자열(문자리스트))
+        int count_translate = 0;
+        int count_zero = 0;
 
-    
-        while (!(s.length == 1 && s[0] == '1' )){ // s != 1 동안
+        while (!s.equals("1")) {
             
-            // 문자열 s (0 제거 전) -> 문자열 x (0 제거 후)
-            for (char c : s) {
-                if (!(c == '0'))
-                    x.add(c);
-                else
-                    count_zero++;
-            }
-            s = Integer.toBinaryString(x.size()).toCharArray(); // 문자열 x 사이즈를 2진수로 변환하여 문자열 s에 저장
-            x.clear(); // 문자열 x 초기화
-            count_translate++;
+            int originalLength = s.length(); // 0 제거 전 문자열 길이
+            s = s.replaceAll("0", "");
+            int newLength = s.length(); // 0 제거 후 문자열 길이
+            
+            count_zero += originalLength - newLength; // 0의 개수
+            
+            s = Integer.toBinaryString(newLength); // 0 제거 후 문자열 길이를 2진수로 변환하여 다음 입력 값으로 저장
+            
+            count_translate ++;
         }
         
         int[] answer = new int[]{count_translate, count_zero};
-        return answer;
+        return answer;  
     }
-
 }
