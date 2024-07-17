e09dff7d65e32b4d9132683fb9fa464528686044
Joy <151157127+EunjeongHeo@users.noreply.github.com>
2024-07-17 10:40:57 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 13.34 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
+class Solution {
+    public static void main(String[] args){
+        
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        
+        Solution sol = new Solution();
+        int[] answer = sol.solution(s);
+        
+        System.out.println("["+answer[0]+", "+answer[1]+"]");
+        
+    }
+    
+    
+    public int[] solution(String S) {
+        
+        int count_zero = 0;
+        int count_translate = 0;
+        
+        char[] s = S.toCharArray(); // 0 제거 전 문자열(문자리스트)
+        ArrayList<Character> x = new ArrayList<>(); // 0 제거 후 문자열(문자리스트))
+
+    
+        while (!(s.length == 1 && s[0] == '1' )){ // s != 1 동안
+            
+            // 문자열 s (0 제거 전) -> 문자열 x (0 제거 후)
+            for (char c : s) {
+                if (!(c == '0'))
+                    x.add(c);
+                else
+                    count_zero++;
+            }
+            s = Integer.toBinaryString(x.size()).toCharArray(); // 문자열 x 사이즈를 2진수로 변환하여 문자열 s에 저장
+            x.clear(); // 문자열 x 초기화
+            count_translate++;
+        }
+        
+        int[] answer = new int[]{count_translate, count_zero};
+        return answer;
+    }
+
+}
