86c42ce3b2f2a77d10548db04dfda2cd7c24048c
Joy <151157127+EunjeongHeo@users.noreply.github.com>
2024-07-21 14:31:04 +0900

    [level 1] Title: 신규 아이디 추천, Time: 3.88 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -2,42 +2,38 @@ import java.util.ArrayList;
 
 class Solution {
     public String solution(String new_id) {
-
-
-        String old_id = new_id;
-
+        
         // 1단계
         new_id = new_id.toLowerCase();
-
+        
         // 2단계
-        new_id = new_id.replaceAll("[^a-z0-9-_.]","");
-
+        new_id = new_id.replaceAll("[^a-z0-9\\-\\_\\.]","");
+        
         // 3단계
-        new_id = new_id.replaceAll("\\.{2,}",".");
-
+        new_id = new_id.replaceAll("[\\.]{2,}",".");
+        
         // 4단계
-        new_id = new_id.replaceAll("(^\\.|\\.$)","");
-
+        new_id = new_id.replaceAll("^[\\.]|[\\.]$","");
+        
         // 5단계
-        if (new_id.equals("")){
+        if(new_id.equals("")){
             new_id = "a";
         }
-
+        
         // 6단계
-        if(new_id.length() >= 16){
-            new_id = new_id.substring(0, 15);
-            new_id = new_id.replaceAll("\\.$", "");
+        if (new_id.length() >= 16){
+            new_id = new_id.substring(0,15);
+            if (new_id.substring(14,15).equals("."))
+                new_id = new_id.substring(0,14);
         }
-
+        
         // 7단계
-        if(new_id.length() <= 2) {
-            StringBuilder new_idBuilder = new StringBuilder(new_id);
-            while (new_idBuilder.length() < 3) {
-                new_idBuilder.append(new_idBuilder.charAt(new_idBuilder.length() - 1));
+        if (new_id.length() <= 2){
+            while(new_id.length() < 3){
+                new_id = new_id + new_id.substring(new_id.length()-1, new_id.length());
             }
-            new_id = new_idBuilder.toString();
         }
-
+        
         return new_id;
     }
 }
