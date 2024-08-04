8819202c67b0f1e1fdaf564b24688494ab39a343
Joy <151157127+EunjeongHeo@users.noreply.github.com>
2024-08-04 16:18:02 +0900

    [level 2] Title: 카펫, Time: 0.13 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,12 +1,27 @@
-import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int a = (brown+4)/2;
-        int b = yellow + 2*a-4;
-        int[] answer = {
-		        (int)(a+Math.sqrt(a*a-4*b))/2,
-		        (int)(a-Math.sqrt(a*a-4*b))/2
-        };
-        return answer;
+        
+        int brownRow = 3; // 행 개수 (세로 길이)
+        int brownCol = 3; // 열 개수 (가로 길이)
+        int yellowRow = 1; // 행 개수 (세로 길이)
+        int yellowCol = 1; // 열 개수 (가로 길이)
+        
+        for (int i = 1; i < yellow ; i++){
+            
+            // yellow 개수 고려 -> yellow 가로,세로 길이 세팅
+            yellowRow = i;
+            if (yellow%yellowRow == 0)
+                yellowCol = yellow/yellowRow;
+            
+            // yellow의 가로,세로 길이 고려 -> brown 가로,세로 길이 세팅
+            brownRow = yellowRow + 2;
+            brownCol = yellowCol + 2;
+            
+            // 조건 필터
+            if (brown == (brownRow * brownCol - yellowRow * yellowCol)) // 총 가로,세로 길이 고려 -> brown 개수 확인
+                return new int[]{brownCol, brownRow};;
+        }
+        
+        return new int[]{brownCol, brownRow};
     }
 }
