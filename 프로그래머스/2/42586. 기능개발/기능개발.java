6483f92a2b3517e9a9029bc6bff1a55cf3f7ea5d
Joy <151157127+EunjeongHeo@users.noreply.github.com>
2024-08-06 02:19:35 +0900

    [level 2] Title: 기능개발, Time: 3.79 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,56 +1,15 @@
-import java.util.*;
-    
+import java.util.ArrayList;
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        
-        // 필요한 일수를 계산한 배열 만들기
-        Integer[] days = new Integer[progresses.length];
-        for( int i = 0; i < progresses.length ; i++){
-            days[i] = (int)Math.ceil((100 - (double)progresses[i]) / speeds[i]);
-        }
-        
-        
-        // 정답 구하기
-        ArrayList<Integer> answer = new ArrayList<>();
-        for( int i = 0; i < days.length ; i++){
-            
-            // 이미 배포한 기능이라면, 넘어감
-            if(days[i]==null){
-                continue;
+        int[] dayOfend = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) {
+                day++;
             }
-            
-            // 마지막 기능인데 배포가 안 되어 있다면,
-            // 또는 현재 검사중인 i번째 기능이 그 다음 기능보다 먼저 완료된다면, 기능 1개 배포 처리
-            if (i == days.length-1 || days[i]<days[i+1]){
-                answer.add(1);
-                days[i] = null;
-            }
-            
-            // 현재 검사중인 i번째 기능보다 그 다음 기능이 먼저 완료된다면, 일괄 배포 처리
-            else{
-                int count = 1;
-                for( int j=i+1; j < days.length ; j++){
-                    if(days[i]>=days[j]){
-                        count++;
-                        days[j] = null;
-                    }
-                    else{
-                        break;
-                    }
-                }
-                answer.add(count);
-                days[i] = null;
-            }
-            
-            
-            
+            dayOfend[day]++;
         }
-        
-        int[] answerArr = new int[answer.size()];
-        for(int i = 0; i <answer.size(); i++){
-            answerArr[i] = answer.get(i);
-        }
-        
-        return answerArr;
+        return Arrays.stream(dayOfend).filter(i -> i!=0).toArray();
     }
 }
