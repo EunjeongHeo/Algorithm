5ee004cc6a93aeff384139b01367208fb25c10bc
Joy <151157127+EunjeongHeo@users.noreply.github.com>
2024-08-07 16:38:54 +0900

    [level 2] Title: 올바른 괄호, Time: 19.92 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/README.md
+++ 프로그래머스/2/12909. 올바른 괄호/README.md
@@ -1,10 +1,10 @@
 # [level 2] 올바른 괄호 - 12909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12909) 
 
 ### 성능 요약
 
-메모리: 52.4 MB, 시간: 6.25 ms
+메모리: 52.8 MB, 시간: 19.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 07일 16:23:09
+2024년 08월 07일 16:38:52
 
 ### 문제 설명
 
--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,34 +1,22 @@
-class Solution {
-    boolean solution(String s) {
-        
-        int count_left = 0; // ( 개수
-        int count_right = 0;  // ) 개수
+import java.util.*;  
+  
+class Solution {  
+  
+    boolean solution(String s) {  
     
-        char[] arr = s.toCharArray();
+        Stack<Character> stack = new Stack<>();  
         
-
-        //  괄호 () 쌍이 올바르게 종료된 후 ')' 가 나오면 false 처리
-        for (int i = 0; i < arr.length; i++){
-            
-            // (, ) 각각의 개수가 같을 때, 다음 문자가 ')' 라면 false 처리
-            if((count_left == count_right) && (i!= arr.length -1 ) && arr[i] == ')'){
-                return false;
-            }
-            
-            // (, ) 각각의 개수 카운트
-            if(arr[i] == '(')
-                count_left++;
-            else
-                count_right++;
-            
+        for (int i = 0; i < s.length(); i++) {  
+        
+            if (s.charAt(i) == '(') {  
+                stack.push('(');  
+            } else if (s.charAt(i) == ')') {  
+                if (stack.isEmpty()) {  
+                    return false;  
+                }  
+                stack.pop();  
+            }  
         }
-        
-        // (, ) 쌍의 개수가 같지 않으면 false 처리
-        if(count_left != count_right)
-            return false;
-        
-        // 그 외의 경우에는 모두 true
-        return true;
-        
-    }
+        return stack.isEmpty();  
+    }  
 }
