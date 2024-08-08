906ec1713f4463a21d2004695f44c007b7499354
Joy <151157127+EunjeongHeo@users.noreply.github.com>
2024-08-08 19:54:07 +0900

    [level 2] Title: 프로세스, Time: 0.99 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -3,9 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         
-        
-        
-        
         Queue<Integer> rq = new LinkedList<>(); // 우선순위 레디 큐
         int[] num = new int[10]; // 인덱스에 해당하는 우선순위의 개수를 담는 배열
         for( int p : priorities){
@@ -16,13 +13,13 @@ class Solution {
         int count = 0; // 제거된 원소 개수 카운트
         
         do{
-            boolean check = false; // 큐에 다시 삽입 되었는지 여부
+            boolean check = true; // 현재 원소 우선순위가 가장 높은지 여부
             
             // 현재 원소보다 우선순위가 더 높은 게 있다면 큐에서 제거 후 다시 삽입
             for (int i = rq.peek() + 1 ; i < num.length; i++){
                 if(num[i] > 0){ 
                     rq.offer(rq.poll());
-                    check = true;
+                    check = false;
                     if(location == 0){
                         location = rq.size()-1;
                     }
@@ -34,7 +31,7 @@ class Solution {
             }
             
             // 현재 원소가 우선순위가 가장 높은 것이라면 큐에서 제거
-            if (check == false){
+            if (check == true){
                 num[rq.poll()]--;
                 count++;
                 if(location == 0){
